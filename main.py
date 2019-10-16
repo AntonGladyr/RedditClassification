22dc24a0b2adf80887d6b71c262713aafa8c9ff2
Anton Gladyr <anton.gladyr@gmail.com>
2019-10-16 16:01:51 -0400

    minor fixes

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import numpy as np
 from data_reader import read_data
 from data_preprocessor import preprocess_data
 from sklearn.feature_extraction.text import TfidfVectorizer
-# import nltk
-# nltk.download('stopwords')
-# from nltk.corpus import stopwords
+from nltk.corpus import stopwords
 from sklearn.feature_extraction import stop_words
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -41,35 +39,65 @@ def main():
     y = le.fit_transform(y)
     X = preprocess_data(X)
 
-    tfidfconverter = TfidfVectorizer(ngram_range=(1, 2), min_df=5, max_df=0.75, stop_words=stop_words.ENGLISH_STOP_WORDS)
+    stopword_list = stop_words.ENGLISH_STOP_WORDS.union(stopwords.words('english'))
+    tfidfconverter = TfidfVectorizer(ngram_range=(1, 3), min_df=5, max_df=0.75,
+                                     stop_words=stopword_list)
     X = tfidfconverter.fit_transform(X).toarray()
-
-    print(tfidfconverter.get_feature_names())
+    with open('csvfile.txt', 'w') as f:
+        for item in tfidfconverter.get_feature_names():
+            f.write("%s\n" % item)
+    # print(tfidfconverter.get_feature_names())
     print(X.shape)
 
     X = csr_matrix(X)
+    print('Normalized features')
+    X = preprocessing.normalize(X)
     print(X.shape)
-    # #TruncatedSVD
-    print('TruncatedSVD')
-    svd = TruncatedSVD(n_components=20, n_iter=7, random_state=42)
-    svd.fit(X)
+    # print('Logistic regression, whole dataset')
+    # skfold = model_selection.StratifiedKFold(n_splits=5)
+    # model = LogisticRegression(random_state=0, solver='saga', multi_class='multinomial')
+    # cv_result = model_selection.cross_val_score(model, X, y, cv=skfold, scoring='accuracy')
+    # msg = "%s: %f" % ('Logistic regression', cv_result.mean())
+    # print(msg)
 
-    models = [('LR', LogisticRegression(random_state=0, solver='saga', multi_class='multinomial')),
-              ('DTC', tree.DecisionTreeClassifier()),
-              ('SVM', svm.SVC(gamma='scale', decision_function_shape='ovo'))]
-
-    # evaluate each model in turn
+    print('------------')
     results = []
     names = []
     scoring = 'accuracy'
-
+    models = [('LR', LogisticRegression(random_state=0, solver='saga', multi_class='multinomial')),
+              ('DTC', tree.DecisionTreeClassifier())]
+    results = []
+    names = []
+    # #TruncatedSVD
+    # print('TruncatedSVD n_components={0}'.format(700))
+    # svd = TruncatedSVD(n_components=700)
+    # X_truncated = svd.fit_transform(X)
+    skfold = model_selection.StratifiedKFold(n_splits=5)
     for name, model in models:
-        kfold = model_selection.KFold(n_splits=5)
-        cv_results = model_selection.cross_val_score(model, X, y, cv=kfold, scoring=scoring)
+        cv_results = model_selection.cross_val_score(model, X, y, cv=skfold, scoring=scoring)
         results.append(cv_results)
         names.append(name)
         msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
         print(msg)
+        # X = csr_matrix(X)
+        # print(X.shape)
+
+    # models = [('LR', LogisticRegression(random_state=0, solver='saga', multi_class='multinomial')),
+    #           ('DTC', tree.DecisionTreeClassifier()),
+    #           ('SVM', svm.SVC(kernel='linear'))]
+    #
+    # # evaluate each model in turn
+    # results = []
+    # names = []
+    # scoring = 'accuracy'
+    #
+    # for name, model in models:
+    #     kfold = model_selection.KFold(n_splits=5)
+    #     cv_results = model_selection.cross_val_score(model, X, y, cv=kfold, scoring=scoring)
+    #     results.append(cv_results)
+    #     names.append(name)
+    #     msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    #     print(msg)
 
 
 if __name__ == '__main__':
